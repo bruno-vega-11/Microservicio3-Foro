43231d190ec79eef420b4253664547c14f862fd6
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-04-17 19:23:24 -0500

    Update ThreadService

--- src/main/java/com/example/foroservice/Application/Services/ThreadService.java
+++ src/main/java/com/example/foroservice/Application/Services/ThreadService.java
@@ -3,6 +3,8 @@ package com.example.foroservice.Application.Services;
 import com.example.foroservice.Domain.Entities.ForumThread;
 import com.example.foroservice.Domain.Repositories.ThreadRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -18,12 +20,20 @@ public class ThreadService {
         return threadRepository.findAll();
     }
 
+    public Page<ForumThread> getAllPaginated(Pageable pageable){
+        return threadRepository.findAll(pageable);
+    }
+
     public ForumThread getById(String id){
         return threadRepository.findById(id).orElseThrow(()->new RuntimeException("Thread not found"));
     }
 
     public List<ForumThread> getByMovieId(String movieId){
         return threadRepository.findByMovieId(movieId);
+    }
+
+    public Page<ForumThread> getByMovieIdPaginated(String movieId, Pageable pageable){
+        return threadRepository.findByMovieId(movieId, pageable);
     }
 
     public ForumThread create(ForumThread thread){
