4e44202e937a3fdb3f04b1fd9ee2169532c1571f
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-04-17 19:24:55 -0500

    Update PostRepository

--- src/main/java/com/example/foroservice/Domain/Repositories/PostRepository.java
+++ src/main/java/com/example/foroservice/Domain/Repositories/PostRepository.java
@@ -1,6 +1,8 @@
 package com.example.foroservice.Domain.Repositories;
 
 import com.example.foroservice.Domain.Entities.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post,String> {
     List<Post> findByThreadId(String threadId);
     List<Post> findByUserId(String userId);
+    Page<Post> findAll(Pageable pageable);
+    Page<Post> findByThreadId(String threadId, Pageable pageable);
+    Page<Post> findByUserId(String userId, Pageable pageable);
 }
