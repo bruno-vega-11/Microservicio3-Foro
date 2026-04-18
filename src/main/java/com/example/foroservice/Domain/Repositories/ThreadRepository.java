51344a0c549974c73d85ecc86c259aad14fab803
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-04-17 19:25:26 -0500

    Update threadRepository

--- src/main/java/com/example/foroservice/Domain/Repositories/ThreadRepository.java
+++ src/main/java/com/example/foroservice/Domain/Repositories/ThreadRepository.java
@@ -1,5 +1,7 @@
 package com.example.foroservice.Domain.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.example.foroservice.Domain.Entities.ForumThread;
@@ -10,4 +12,7 @@ import java.util.List;
 public interface ThreadRepository extends MongoRepository<ForumThread, String> {
     List<ForumThread> findByMovieId(String movideId);
     List<ForumThread> findByUserId(String userId);
+    Page<ForumThread> findAll(Pageable pageable);
+    Page<ForumThread> findByMovieId(String movieId, Pageable pageable);
+    Page<ForumThread> findByUserId(String userId, Pageable pageable);
 }
